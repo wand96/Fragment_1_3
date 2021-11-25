75cdfa434f0454758b36f5e91b4f40d47f0de758
wand96 <tjsrb96@naver.com>
2021-11-25 14:57:21 +0900

    수신측의 코드 작성 완료

--- app/src/main/java/com/example/fragment_1_3/RecieverFragment.kt
+++ app/src/main/java/com/example/fragment_1_3/RecieverFragment.kt
@@ -5,55 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.setFragmentResultListener
+import com.example.fragment_1_3.databinding.FragmentRecieverBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RecieverFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RecieverFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    lateinit var binding:FragmentRecieverBinding
+    //다른 매서드에서도 사용하기 위해 onCreateView 메서드 밖에 바인딩 생성
+    //onCreateView() 메서드 안에서만 사용할 수 있는 파라미터가 필요하므로 미리 lateinit으로 선언만하고 진행
+    
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reciever, container, false)
+        binding = FragmentRecieverBinding.inflate(inflater, container, false)
+        //onCreaateView() 메서드 안에서 바인딩을 생성해서 binding프로퍼티에 저장
+        //binding프로퍼티에 바인딩을 저장했기 때문에 다른 메서드에서도 가져다 쓸 수 있음
+        return binding.root
+        //binding.root를 반환
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RecieverFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RecieverFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        //onViewCreated 메서드를 오버라이드
+        super.onViewCreated(view, savedInstanceState)
+        
+        setFragmentResultListener("request") { key, bundle ->
+        //보내는 측 프래그먼트에서 "request"라는 키로 값을 보내면 이 리스너 안의 코드가 실행
+        //리스너는 값을 수신하면 key와 bundle 2개의 파라미터를 사용 
+            //실제 값은 bundle안에 Map의 형태로 담겨 있음
+            //bundle.getString("키")로 값을 꺼낼 수 있음
+            
+            bundle.getString("valueKey")?.let {
+            //꺼낸 값이 있을 때만 textView에 값을 세팅
+                binding.textView.setText(it)
+        }
+        //request는 요청 전체에 대한 키
+            //bundle.getString에 입력되는 "valuekey"는 요청에 담겨있는 값 중 하나의 값을 지칭
+        }
     }
 }
